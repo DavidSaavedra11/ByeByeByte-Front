2e6d29fd5c569c1e984e4030291b167db6fbae18
David Saavedra <david.d11@hotmail.com>
2021-12-06 23:14:02 -0500

    Estilos Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,38 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const SidebarLinks = () => {
   return (
-    <ul className='mt-12'>
-      <SidebarRoute to='' title='Inicio' icon='fas fa-home' />
-      <SidebarRoute to='/usuarios' title='Usuarios' icon='fas fa-user' />
-      <SidebarRoute to='/page2' title='Proyectos' icon='fas fa-project-diagram' />
-      <SidebarRoute to='/category1' title='Inscripciones' icon='fas fa-newspaper' />
-      <SidebarRoute to='/category1/page1' title='Avances' icon='fas fa-file-alt' />
+    <ul className="mt-6">
+      <SidebarRoute to="" title="Inicio" icon="fas fa-home" />
+
+      <SidebarRoute
+        to="/usuarios"
+        title="Usuarios"
+        icon="fas fa-user-astronaut"
+      />
+      <SidebarRoute to="/page2" title="Proyectos" icon="fas fa-space-shuttle" />
+      <SidebarRoute
+        to="/category1"
+        title="Inscripciones"
+        icon="fas fa-meteor"
+      />
+      <SidebarRoute
+        to="/category1/page1"
+        title="Avances"
+        icon="fas fa-globe-americas"
+      />
     </ul>
   );
 };
 
 const Logo = () => {
   return (
-    <div className='py-3 w-full flex flex-col items-center justify-center'>
-      <img src='logo.png' alt='Logo' className='h-16' />
-      <span className='my-2 text-xl font-bold text-center'>Gestor de Proyectos ByeByeByte</span>
+    <div className="py-3 w-full flex flex-col items-center justify-center">
+      <img src="logo.png" alt="Logo" className="h-18" />
+      <span className=" text-xl font-bold text-center">
+        Gestor de Proyectos ByeByeByte
+      </span>
     </div>
   );
 };
@@ -25,18 +40,21 @@ const Logo = () => {
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
   return (
-    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full'>
+    <div className="border-r-4 border-t-4 rounded-r-xl border-red-600   flex flex-col md:flex-row flex-no-wrap md:h-full">
       {/* Sidebar starts */}
 
-      <div className='sidebar hidden md:flex'>
-        <div className='px-8'>
+      <div className=" rounded-r-lg sidebar hidden md:flex">
+        <div className="px-8">
           <Logo />
           <SidebarLinks />
         </div>
       </div>
-      <div className='flex md:hidden w-full justify-between bg-gray-800 p-2 text-white'>
-        <i className={`fas fa-${open ? 'times' : 'bars'}`} onClick={() => setOpen(!open)} />
-        <i className='fas fa-home' />
+      <div className="flex md:hidden w-full justify-between bg-gray-800 p-2 text-white">
+        <i
+          className={`fas fa-${open ? "times" : "bars"}`}
+          onClick={() => setOpen(!open)}
+        />
+        <i className="fas fa-home" />
       </div>
       {open && <ResponsiveSidebar />}
       {/* Sidebar ends */}
@@ -48,10 +66,10 @@ const ResponsiveSidebar = () => {
   return (
     <div>
       <div
-        className='sidebar h-full z-40 absolute md:h-full sm:hidden transition duration-150 ease-in-out'
-        id='mobile-nav'
+        className="sidebar h-full z-40 absolute md:h-full sm:hidden transition duration-150 ease-in-out"
+        id="mobile-nav"
       >
-        <div className='px-8'>
+        <div className="px-8">
           <Logo />
           <SidebarLinks />
         </div>
@@ -67,13 +85,13 @@ const SidebarRoute = ({ to, title, icon }) => {
         to={to}
         className={({ isActive }) =>
           isActive
-            ? 'sidebar-route text-white bg-yellow-700'
-            : 'sidebar-route text-black-600 hover:text-white hover:bg-yellow-600'
+            ? " border-b-2 border-black  sidebar-route text-white bg-yellow-700 font-bold"
+            : " bg-red-600 border-2 border-red-800 sidebar-route text-black-600 font-bold text-xl hover:text-white hover:bg-yellow-600"
         }
       >
-        <div className='flex items-center'>
+        <div className="flex items-center">
           <i className={icon} />
-          <span className='text-sm  ml-2'>{title}</span>
+          <span className="text-sm  ml-6">{title}</span>
         </div>
       </NavLink>
     </li>
